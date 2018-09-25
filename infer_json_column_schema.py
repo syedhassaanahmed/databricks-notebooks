3d2c6de7d43739951eed02457b1534a9196465c1
Syed Hassaan Ahmed <syah@microsoft.com>
2018-09-25 23:25:36 +0200

    Add infer nested json string schema example

---
+++ infer_json_column_schema.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import json
+import requests
+
+# COMMAND ----------
+
+exampleJSON = """
+{
+  "a": "{\\"b\\":{\\"x\\":1,\\"y\\":{\\"z\\":2}}}"
+}
+"""
+
+dfExampleJSON = spark.read.json(sc.parallelize([exampleJSON]))
+display(dfExampleJSON)
+
+# COMMAND ----------
+
+def get_json_schema(df, jsonColumnName):
+  firstRecordString = df.where(col(jsonColumnName).isNotNull()).first()[jsonColumnName]
+  firstRecordRDD = sc.parallelize([firstRecordString])
+  dfJSON = spark.read.json(firstRecordRDD)
+  schema = dfJSON.schema.json()
+  dictionary = json.loads(schema)
+  
+  return StructType.fromJson(dictionary)
+
+# COMMAND ----------
+
+schema = get_json_schema(dfExampleJSON, "a")
+dfExampleStruct = dfExampleJSON.withColumn("a", from_json("a", schema))
+
+display(dfExampleStruct)
