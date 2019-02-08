78b590e4292a58b34a2139fdac7c0c8ab9bbf6f4
Syed Hassaan Ahmed <syah@microsoft.com>
2019-02-08 02:04:34 +0100

    Replace SQL snippet with PySpark

--- generate_large_time_series_csv.py
+++ generate_large_time_series_csv.py
@@ -1,31 +1,26 @@
 # Databricks notebook source
-from pyspark.sql.functions import *
+import pyspark.sql.functions as F
 import time
 
-days_back = 1
+# COMMAND ----------
+
+days_back = 14
 values_per_second = 337
-
-sqlContext.range(0, days_back * 24 * 60 * 60 * values_per_second) \
-  .withColumn("seconds", col("id") / values_per_second) \
-  .withColumn("nowTimestamp", lit(time.time())) \
-  .createOrReplaceTempView("all_rows")
+nowTimestamp = time.time()
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC CREATE OR REPLACE TEMP VIEW TimeSeries AS
-# MAGIC SELECT
-# MAGIC   CAST((nowTimestamp - seconds) AS Timestamp) AS Timestamp
-# MAGIC   , concat_ws('-', 1 + CAST(rand() * 10 AS Int), 1 + CAST(rand() * 100 AS Int), 1 + CAST(rand() * 350 AS Int)) AS Sensor
-# MAGIC   , round(rand() * 100, 3) AS Value
-# MAGIC FROM all_rows
-
-# COMMAND ----------
-
-dfTimeSeries = spark.table("TimeSeries") \
-  .withColumn("year", year(col("Timestamp"))) \
-  .withColumn("month", month(col("Timestamp"))) \
-  .withColumn("day", dayofmonth(col("Timestamp")))
+dfTimeSeries = sqlContext.range(0, days_back * 24 * 60 * 60 * values_per_second) \
+  .withColumn("Timestamp", (nowTimestamp - (F.col("id") / values_per_second)).cast("Timestamp")) \
+  .drop("id") \
+  .withColumn("Sensor", F.concat_ws('-', 
+                               1 + (F.rand() * 10).cast("Int"), 
+                               1 + (F.rand() * 100).cast("Int"), 
+                               1 + (F.rand() * 350).cast("Int"))) \
+  .withColumn("Value", F.round(F.rand() * 100, 3)) \
+  .withColumn("year", F.year("Timestamp")) \
+  .withColumn("month", F.month("Timestamp")) \
+  .withColumn("day", F.dayofmonth("Timestamp"))
 
 display(dfTimeSeries)
 
