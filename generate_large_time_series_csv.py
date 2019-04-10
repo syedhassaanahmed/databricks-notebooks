05da889c7f5b37e1e894e9ab6ea4b2ecb8177601
Syed Hassaan Ahmed <syah@microsoft.com>
2019-04-10 22:41:10 +0200

    Use guids as Sensor IDs

--- generate_large_time_series_csv.py
+++ generate_large_time_series_csv.py
@@ -1,34 +1,52 @@
 # Databricks notebook source
-# MAGIC %md ## Generate Large Timeseries CSV
-# MAGIC For best performance we used 5 instances of `Standard_DS15_v2` (Memory Optimized) as Worker Node
+# MAGIC %md #### For best performance use 5 instances of `Standard_DS15_v2` (Memory Optimized) as Worker Node
 
 # COMMAND ----------
 
 import pyspark.sql.functions as F
+from pyspark.sql.window import Window
 import time
-
-# COMMAND ----------
 
 days_back = 120
 industrial_plants = 50
+
 start_time = time.time()
 
 # COMMAND ----------
 
-def generate_timeseries(start_time, days_back, industrial_plants, sensor_type, sensors_per_plant, values_per_second_per_sensor):
-  values_per_second = industrial_plants * sensors_per_plant * values_per_second_per_sensor  
-  
-  return spark.range(0, days_back * 24 * 60 * 60 * values_per_second, 1, 80000) \
+def get_sensors(industrial_plants, sensors_per_plant):
+  return spark.range(0, industrial_plants * sensors_per_plant) \
+    .selectExpr("uuid() AS Sensor") \
+    .withColumn("RowNum", F.row_number().over(Window.orderBy("Sensor"))) \
+    .withColumn("plant", F.col("RowNum") % industrial_plants)
+
+# COMMAND ----------
+
+def generate_timeseries(df_sensors, start_time, days_back, sensor_frequency):
+  total_sensors = df_sensors.count()
+  values_per_second = total_sensors * sensor_frequency
+    
+  return spark.range(0, days_back * 24 * 60 * 60 * values_per_second, 1, 8000) \
+    .withColumn("RowNum", 1 + (F.col("id") % total_sensors)) \
+    .join(df_sensors, "RowNum") \
     .withColumn("Timestamp", (start_time - (F.col("id") / values_per_second)).cast("Timestamp")) \
-    .withColumn("Tag", F.concat_ws('-', 
-                                 1 + (F.col("id") / sensors_per_plant % industrial_plants).cast("Int"), 
-                                 F.lit(sensor_type), 
-                                 1 + (F.col("id") % sensors_per_plant))) \
     .withColumn("Value", F.round(F.rand() * 100, 16)) \
     .withColumn("year", F.year("Timestamp")) \
     .withColumn("month", F.month("Timestamp")) \
     .withColumn("day", F.dayofmonth("Timestamp")) \
-    .drop("id")
+    .drop("id", "RowNum")
+
+# COMMAND ----------
+
+sensors_per_plant = 5000
+df_sensors = get_sensors(industrial_plants, sensors_per_plant)
+display(df_sensors)
+
+# COMMAND ----------
+
+sensor_frequency = 1/10
+dfTimeseries = generate_timeseries(df_sensors, start_time, days_back, sensor_frequency)
+display(dfTimeseries)
 
 # COMMAND ----------
 
@@ -39,26 +57,7 @@ blob_root = "wasbs://<StorageContainer>@<StorageAccountName>.blob.core.windows.n
 
 # COMMAND ----------
 
-dfTimeseriesA = generate_timeseries(start_time, days_back, industrial_plants, "A", 5000, 1/3)
-#display(dfTimeseriesA)
-
-# COMMAND ----------
-
-dfTimeseriesA.write \
+dfTimeseries.write \
   .mode("overwrite") \
-  .option("compression", "gzip") \
-  .partitionBy("year", "month", "day") \
-  .csv(blob_root + "/A")
-
-# COMMAND ----------
-
-dfTimeseriesB = generate_timeseries(start_time, days_back, industrial_plants, "B", 10000, 1/10)
-#display(dfTimeseriesB)
-
-# COMMAND ----------
-
-dfTimeseriesB.write \
-  .mode("overwrite") \
-  .option("compression", "gzip") \
-  .partitionBy("year", "month", "day") \
-  .csv(blob_root + "/B")
+  .partitionBy("plant", "year", "month", "day") \
+  .csv(blob_root)
